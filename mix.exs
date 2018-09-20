f8e8824c2efbd82ec79dcedc6b97bb5b88b23ec8
chenxsan <chenxsan@gmail.com>
2018-09-20 21:56:04 +0800

    Fix warning

--- mix.exs
+++ mix.exs
@@ -54,7 +54,7 @@ defmodule TweetBot.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
