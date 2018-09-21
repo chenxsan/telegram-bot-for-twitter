7a2896f58057d384511d24b3216d6f35b74aa750
chenxsan <chenxsan@gmail.com>
2018-09-21 21:02:28 +0800

    Create User

--- lib/tweet_bot_web/controllers/auth_controller.ex
+++ lib/tweet_bot_web/controllers/auth_controller.ex
@@ -1,20 +1,22 @@
 defmodule TweetBotWeb.AuthController do
   use TweetBotWeb, :controller
+  alias TweetBot.Accounts
 
-  def callback(conn, %{"oauth_token" => oauth_token, "oauth_verifier" => oauth_verifier}) do
+  def callback(conn, %{
+        "from_id" => from_id,
+        "oauth_token" => oauth_token,
+        "oauth_verifier" => oauth_verifier
+      }) do
     # 获取 access token
     {:ok, token} = ExTwitter.access_token(oauth_verifier, oauth_token)
 
-    ExTwitter.configure(
-      :process,
-      Enum.concat(
-        ExTwitter.Config.get_tuples(),
-        access_token: token.oauth_token,
-        access_token_secret: token.oauth_token_secret
-      )
-    )
-
-    ExTwitter.update("I just sign up telegram bot tweet_for_me_bot.")
-    text(conn, "授权成功，请关闭此页面")
+    case Accounts.create_user(%{
+           from_id: from_id,
+           access_token: token.oauth_token,
+           access_token_secret: token.oauth_token_secret
+         }) do
+      {:ok, _} -> text(conn, "授权成功，请关闭此页面")
+      {:error, reason} -> text(conn, "授权失败：#{reason}")
+    end
   end
 end
