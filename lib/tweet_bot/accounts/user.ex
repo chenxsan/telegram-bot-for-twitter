37fbf3da52a3391fe7e5f14826d0701f86d3e5c5
chenxsan <chenxsan@gmail.com>
2018-09-21 12:52:23 +0800

    Add smoke test

--- lib/tweet_bot/accounts/user.ex
+++ lib/tweet_bot/accounts/user.ex
@@ -2,10 +2,9 @@ defmodule TweetBot.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
-
   schema "users" do
-    field :access_token, :string
-    field :from_id, :string
+    field(:access_token, :string)
+    field(:from_id, :string)
 
     timestamps()
   end
@@ -14,7 +13,7 @@ defmodule TweetBot.Accounts.User do
   def changeset(user, attrs) do
     user
     |> cast(attrs, [:from_id, :access_token])
-    |> validate_required([:from_id, :access_token])
+    |> validate_required([:from_id])
     |> unique_constraint(:from_id)
   end
 end
--- test/tweet_bot/accounts/user_test.exs
+++ test/tweet_bot/accounts/user_test.exs
@@ -2,4 +2,17 @@ defmodule TweetBot.UserTest do
   use TweetBot.DataCase
 
   alias TweetBot.Accounts.User
+
+  @valid_attrs %{from_id: "123456"}
+  @invalid_attrs %{from_id: nil}
+
+  test "changeset with valid attributes" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = User.changeset(%User{}, @invalid_attrs)
+    refute changeset.valid?
+  end
 end
