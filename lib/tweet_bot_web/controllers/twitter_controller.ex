35c6f67e811b3ce14150992a9199620ddf528d96
chenxsan <chenxsan@gmail.com>
2018-09-21 20:07:04 +0800

    Handle /start

--- lib/tweet_bot_web/controllers/twitter_controller.ex
+++ lib/tweet_bot_web/controllers/twitter_controller.ex
@@ -4,7 +4,25 @@ defmodule TweetBotWeb.TwitterController do
   import TelegramBot
 
   def index(conn, %{"message" => %{"from" => %{"id" => from_id}, "text" => text}}) do
-    sendMessage(from_id, "你好")
+    case text do
+      "/start" ->
+        token =
+          ExTwitter.request_token(
+            URI.encode_www_form(TweetBotWeb.Router.Helpers.auth_url(conn, :callback))
+          )
+
+        {:ok, authenticate_url} = ExTwitter.authenticate_url(token.oauth_token)
+
+        sendMessage(
+          from_id,
+          "请点击链接登录您的 Twitter 账号授权：<a href='" <> authenticate_url <> "'>登录 Twitter</a>",
+          parse_mode: "HTML"
+        )
+
+      _ ->
+        sendMessage(from_id, "你好")
+    end
+
     json(conn, %{})
   end
 end
