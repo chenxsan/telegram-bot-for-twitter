eb1b4341d549cc9e10d8a16ea52566960f3ecae3
chenxsan <chenxsan@gmail.com>
2018-09-21 09:47:33 +0800

    Format router

--- lib/tweet_bot_web/router.ex
+++ lib/tweet_bot_web/router.ex
@@ -2,25 +2,27 @@ defmodule TweetBotWeb.Router do
   use TweetBotWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", TweetBotWeb do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", TweetBotWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", TweetBotWeb do
+    pipe_through(:api)
+    post("/twitter", TwitterController, :index)
+  end
 end
