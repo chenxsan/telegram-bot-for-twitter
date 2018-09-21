f3b2f3c1127ff1713ee4b2d659b80c2251ac7b56
chenxsan <chenxsan@gmail.com>
2018-09-21 20:46:39 +0800

    Log token

--- lib/tweet_bot_web/controllers/auth_controller.ex
+++ lib/tweet_bot_web/controllers/auth_controller.ex
@@ -1,6 +1,10 @@
 defmodule TweetBotWeb.AuthController do
   use TweetBotWeb, :controller
 
-  def callback(conn, _params) do
+  def callback(conn, %{"oauth_token" => oauth_token, "oauth_verifier" => oauth_verifier}) do
+    # 获取 access token
+    {:ok, token} = ExTwitter.access_token(oauth_verifier, oauth_token)
+    IO.inspect(token)
+    text(conn, "授权成功，请关闭此页面")
   end
 end
