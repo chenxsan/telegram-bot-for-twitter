5137ec721cff7aecfc7d97540e6ba10e0629eb38
chenxsan <chenxsan@gmail.com>
2018-09-25 18:54:47 +0800

    Add mox

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule TweetBot.Mixfile do
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
       {:telegram_bot, "~> 1.0.1"},
-      {:extwitter, "~> 0.9.3"}
+      {:extwitter, "~> 0.9.3"},
+      {:mox, "~> 0.4", only: :test}
     ]
   end
 
