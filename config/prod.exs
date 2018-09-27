6242f589023d4aac83114bba6e6b9133eac1a7eb
chenxsan <chenxsan@gmail.com>
2018-09-27 09:33:17 +0800

    Configures more

--- config/prod.exs
+++ config/prod.exs
@@ -21,6 +21,26 @@ config :tweet_bot, TweetBotWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
+# Configures token for telegram bot
+config :telegram_bot,
+  token: System.get_env("TELEGRAM_TOKEN")
+
+# Configures extwitter oauth
+config :extwitter, :oauth,
+  consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
+  consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET")
+
+config :tweet_bot, TweetBotWeb.Endpoint, secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :tweet_bot, TweetBot.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: System.get_env("DATABASE_USER"),
+  password: System.get_env("DATABASE_PASS"),
+  database: System.get_env("DATABASE_NAME"),
+  hostname: System.get_env("DATABASE_HOST"),
+  pool_size: 15
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
@@ -58,7 +78,3 @@ config :logger, level: :info
 #
 #     config :tweet_bot, TweetBotWeb.Endpoint, server: true
 #
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
