769d2149abfd91c1d05ceea97842f93e3b0c05dc
chenxsan <chenxsan@gmail.com>
2018-10-04 23:43:00 +0800

    0.0.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TweetBot.Mixfile do
   def project do
     [
       app: :tweet_bot,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
