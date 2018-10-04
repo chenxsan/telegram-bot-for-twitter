f8092aef623adfd1d9794eda37846ad46596983f
chenxsan <chenxsan@gmail.com>
2018-10-04 13:20:03 +0800

    Update host

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :tweet_bot, TweetBotWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "localhost", port: {:system, "PORT"}],
+  url: [host: "tweetbot.zfanw.com", port: {:system, "PORT"}],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   root: ".",
