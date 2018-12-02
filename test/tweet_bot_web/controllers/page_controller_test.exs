22e59c45027f3c0e4087d4bcb1eb62ad02509882
chenxsan <chenxsan@gmail.com>
2018-12-02 21:21:33 +0800

    Fix test

--- test/tweet_bot_web/controllers/page_controller_test.exs
+++ test/tweet_bot_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule TweetBotWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Hello Tweet for me bot!"
   end
 end
