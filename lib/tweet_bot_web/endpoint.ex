564d9f995dc74773ecdce6c8b4eb922278ff82ce
chenxsan <chenxsan@gmail.com>
2018-12-03 22:37:04 +0800

    Update json_decoder

--- lib/tweet_bot_web/endpoint.ex
+++ lib/tweet_bot_web/endpoint.ex
@@ -26,7 +26,7 @@ defmodule TweetBotWeb.Endpoint do
   plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
-    json_decoder: Poison
+    json_decoder: Jason
 
   plug Plug.MethodOverride
   plug Plug.Head
