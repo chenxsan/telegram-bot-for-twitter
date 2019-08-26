57eccf099c8d702dfaf597d9c5c94d78a6042b0d
chenxsan <chenxsan@gmail.com>
2019-08-26 19:18:49 +0800

    Update release tasks

--- lib/release_tasks.ex
+++ lib/release_tasks.ex
@@ -1,83 +1,24 @@
-defmodule TweetBot.ReleaseTasks do
-  @start_apps [
-    :crypto,
-    :ssl,
-    :postgrex,
-    :ecto,
-    :ecto_sql
-  ]
+defmodule TweetBot.Release do
+  @app :tweet_bot
 
-  @repos Application.get_env(:tweet_bot, :ecto_repos, [])
+  def migrate do
+    load_app()
 
-  def migrate(_argv) do
-    start_services()
-
-    run_migrations()
-
-    stop_services()
-  end
-
-  def seed(_argv) do
-    start_services()
-
-    run_migrations()
-
-    run_seeds()
-
-    stop_services()
-  end
-
-  defp start_services do
-    IO.puts("Starting dependencies..")
-    # Start apps necessary for executing migrations
-    Enum.each(@start_apps, &Application.ensure_all_started/1)
-
-    # Start the Repo(s) for app
-    IO.puts("Starting repos..")
-    Enum.each(@repos, & &1.start_link(pool_size: 2))
-  end
-
-  defp stop_services do
-    IO.puts("Success!")
-    :init.stop()
-  end
-
-  defp run_migrations do
-    Enum.each(@repos, &run_migrations_for/1)
-  end
-
-  defp run_migrations_for(repo) do
-    app = Keyword.get(repo.config, :otp_app)
-    IO.puts("Running migrations for #{app}")
-    migrations_path = priv_path_for(repo, "migrations")
-    Ecto.Migrator.run(repo, migrations_path, :up, all: true)
-  end
-
-  defp run_seeds do
-    Enum.each(@repos, &run_seeds_for/1)
-  end
-
-  defp run_seeds_for(repo) do
-    # Run the seed script if it exists
-    seed_script = priv_path_for(repo, "seeds.exs")
-
-    if File.exists?(seed_script) do
-      IO.puts("Running seed script..")
-      Code.eval_file(seed_script)
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
 
-  defp priv_path_for(repo, filename) do
-    app = Keyword.get(repo.config, :otp_app)
+  def rollback(repo, version) do
+    load_app()
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
 
-    repo_underscore =
-      repo
-      |> Module.split()
-      |> List.last()
-      |> Macro.underscore()
+  defp repos do
+    Application.fetch_env!(@app, :ecto_repos)
+  end
 
-    priv_dir = "#{:code.priv_dir(app)}"
-
-    Path.join([priv_dir, repo_underscore, filename])
+  defp load_app do
+    Application.load(@app)
   end
 end
